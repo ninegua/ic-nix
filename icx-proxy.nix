3c3b253217640f2f2cad1bfafa18c4b6e42d5101
Paul H. Liu <paul@thev.net>
2022-05-18 15:37:39 -0700

    Add mising file

---
+++ icx-proxy.nix
@@ -0,0 +1,21 @@
+{ pkgs, source }:
+with pkgs;
+let
+  stdenv = llvmPackages_11.libcxxStdenv;
+  linker = callPackage ./nix/static-linker.nix { inherit stdenv; };
+  buildInputs = [ ] ++ lib.optionals stdenv.isDarwin
+    (with darwin.apple_sdk.frameworks; [ DiskArbitration Foundation ]);
+  icx-proxy = rustPlatform.buildRustPackage {
+    name = "icx-proxy";
+    src = source;
+    cargoSha256 = "0q7r5r7vgwqxwmznj1sin8ww9gbkzyg467rqcq22n0297azbshs6";
+    buildInputs = [ openssl-static ]
+      ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ ]);
+    nativeBuildInputs = [ pkg-config ];
+    RUSTFLAGS = lib.optionals stdenv.isDarwin [
+      "-Clinker=${linker}"
+      "-Lnative=${libcxxabi}/lib"
+    ];
+  };
+
+in { inherit icx-proxy; }
