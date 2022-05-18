f8d6bf6a80014ca9bc691cb44ce3f937ba55916d
Paul H. Liu <paul@thev.net>
2022-05-18 08:58:54 -0700

    One more glitch

--- nix/static-linker.nix
+++ nix/static-linker.nix
@@ -4,7 +4,7 @@ writeShellScript "linker.sh" ''
   args=''${args//-lstdc++/}
   args=''${args//-lc++abi/}
   args=''${args//-liconv/}
+  args=''${args//-L \/usr\/local\/opt\/openssl@1.1\/lib/}
   ${stdenv.cc}/bin/c++ -L ${libiconv-static.out}/lib $args \
     -nostdlib++ ${libcxx}/lib/libc++.a ${libcxxabi}/lib/libc++abi.a
 ''
-
