8b64b2eaff42e1292d5327096a9ac9c7512907c7
Paul H. Liu <paul@thev.net>
2022-05-19 23:39:24 -0700

    Detect if -lz is needed

--- nix/static-linker.nix
+++ nix/static-linker.nix
@@ -1,11 +1,12 @@
 { writeShellScript, stdenv, libiconv-static, libcxx, libcxxabi }:
 writeShellScript "linker.sh" ''
+  lz=$(echo "$*"|sed -e 's/ /\n/g'|grep -q '[-]lz')
   args=''${@//-lc++/}
   args=''${args//-lstdc++/}
   args=''${args//-lc++abi/}
   args=''${args//-liconv/}
   args=''${args//-lz/}
   args=''${args//-L \/usr\/local\/opt\/openssl@1.1\/lib/}
-  ${stdenv.cc}/bin/c++ -L ${libiconv-static.out}/lib $args -lz \
+  ${stdenv.cc}/bin/c++ -L ${libiconv-static.out}/lib $args $lz \
     -nostdlib++ ${libcxx}/lib/libc++.a ${libcxxabi}/lib/libc++abi.a
 ''
