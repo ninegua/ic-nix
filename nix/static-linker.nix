93affb2b47173fcf69af36c084f9224cf88b6323
Paul H. Liu <paul@thev.net>
2022-05-19 22:29:27 -0700

    Fix order of linking -lz

--- nix/static-linker.nix
+++ nix/static-linker.nix
@@ -4,7 +4,8 @@ writeShellScript "linker.sh" ''
   args=''${args//-lstdc++/}
   args=''${args//-lc++abi/}
   args=''${args//-liconv/}
+  args=''${args//-lz/}
   args=''${args//-L \/usr\/local\/opt\/openssl@1.1\/lib/}
-  ${stdenv.cc}/bin/c++ -L ${libiconv-static.out}/lib $args \
+  ${stdenv.cc}/bin/c++ -L ${libiconv-static.out}/lib $args -lz \
     -nostdlib++ ${libcxx}/lib/libc++.a ${libcxxabi}/lib/libc++abi.a
 ''
