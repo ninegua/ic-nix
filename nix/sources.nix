c9d0e007b160de770ae9e6f71ba9207033898a57
Paul H. Liu <paul@thev.net>
2022-06-05 22:47:30 -0700

    Fix branch annotation

--- nix/sources.nix
+++ nix/sources.nix
@@ -35,23 +35,23 @@
     sha256 = "1gdcdbg8nqpiy170bhlgzxpz3wdcc8wpxwazsprgz44chghldahr";
   };
   lmdb = fetchgit {
-    url = "https://git.openldap.org/openldap/openldap.git";
-    rev = "55fd54dae6f90080b770dbc9dbcee5044976b7bf"; # mdb.master
+    url = "https://git.openldap.org/openldap/openldap.git"; # mdb.master
+    rev = "55fd54dae6f90080b770dbc9dbcee5044976b7bf";
     sha256 = "0syagyv0jmdmivhlyw5y78rmmm8l175pchiv5g3r1n2mhsk7624j";
   };
   vessel = fetchgit {
-    url = "https://github.com/dfinity/vessel";
-    rev = "13dd83fca5eddf48d63abc7a07cffa4fd44e4f6e"; # main
+    url = "https://github.com/dfinity/vessel"; # main
+    rev = "13dd83fca5eddf48d63abc7a07cffa4fd44e4f6e";
     sha256 = "0n41qjblka2z745n5vskvn9lfa1qs65mfw3196c7r0adb26xmlgz";
   };
   ic-repl = fetchgit {
-    url = "https://github.com/chenyan2002/ic-repl";
-    rev = "41b561942281533f67299e485c4763f3ce91f787"; # master
+    url = "https://github.com/chenyan2002/ic-repl"; # master
+    rev = "41b561942281533f67299e485c4763f3ce91f787";
     sha256 = "1kxqghw2vis2xfjwawasfwsqc3ys1lbn80xgya10ikv6yvpbr05f";
   };
   candid = fetchgit {
-    url = "https://github.com/dfinity/candid";
-    rev = "72b681c3a4fa9cfa1b705e3346118d7eae38ef84"; # master
+    url = "https://github.com/dfinity/candid"; # master
+    rev = "72b681c3a4fa9cfa1b705e3346118d7eae38ef84";
     sha256 = "08p8zwq0m2wkgsc4xfn5822hcn9qk6zsmljnva9dgj716rjknjdz";
   };
 
