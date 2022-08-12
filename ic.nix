a5686d1af37a767a7ce681b1f195b14a6b5db146
Paul Liu <paul@thev.net>
2022-08-12 20:10:37 +0800

    Use customLinker flag to enable/disable custom linker (#23)
    This allows disabling the custom linker in nix-shell.

--- ic.nix
+++ ic.nix
@@ -56,8 +56,8 @@ let
         [ "-Wno-error=deprecated-copy" "-Wno-error=unused-private-field" ];
     });
 
-  buildIC =
-    { targets, hostTriple ? stdenv.hostPlatform.config, profile ? "release" }:
+  buildIC = { customLinker, targets, hostTriple ? stdenv.hostPlatform.config
+    , profile ? "release" }:
     let
       linker = callPackage ./nix/static-linker.nix { inherit stdenv; };
       cargoBuildFlags =
@@ -89,14 +89,14 @@ let
         with darwin.apple_sdk.frameworks; [ CoreServices Foundation Security ]
       else
         [ libunwind ]);
-      cargoSha256 = "sha256-X3scSRSLmGKVCl5nDh9U9BUZuGzQVWrlWBaLLwlOVew="; # cargoSha256
+      cargoSha256 =
+        "sha256-X3scSRSLmGKVCl5nDh9U9BUZuGzQVWrlWBaLLwlOVew="; # cargoSha256
       doCheck = false;
 
       ROCKSDB_LIB_DIR = "${rocksdb}/lib";
       ROCKSDB_INCLUDE_DIR = "${rocksdb}/include";
       LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
-      RUSTFLAGS = [
-        "-Clinker=${linker}"
+      RUSTFLAGS = lib.optionals customLinker [ "-Clinker=${linker}" ] ++ [
         "-Lnative=${libcxxabi}/lib"
         "-Lnative=${zlib-static}/lib"
         "-Lnative=${lmdb.out}/lib"
@@ -118,7 +118,13 @@ let
       '';
     });
 
-  binaries = buildIC { targets = bins ++ wasms; };
+  mkBinaries = { customLinker }:
+    buildIC {
+      targets = bins ++ wasms;
+      inherit customLinker;
+    };
+
+  binaries = mkBinaries { customLinker = true; };
 
   wasm-names = lib.strings.concatStringsSep " " wasms;
 
@@ -126,6 +132,7 @@ let
     targets = wasms;
     hostTriple = "wasm32-unknown-unknown";
     profile = "canister-release";
+    customLinker = false;
   }).overrideAttrs (self: rec {
     name = "ic-wasm";
     RUSTFLAGS = [ ];
@@ -159,5 +166,5 @@ let
   };
 in {
   inherit binaries wasm-binaries canisters;
-  shell = binaries;
+  shell = mkBinaries { customLinker = false; };
 }
