349b1db89435cf7c763121a861eb128d1b5e8d70
Paul H. Liu <paul@thev.net>
2022-08-22 04:05:34 -0700

    Fix sdk test dir

--- sdk.nix
+++ sdk.nix
@@ -17,9 +17,10 @@ let
     inherit buildInputs;
     nativeBuildInputs = [ pkg-config cmake binaryen python3 ];
     preConfigure = ''
-      export DFX_ASSETS=$PWD/dfx_assets
-      mkdir -p $DFX_ASSETS
-      touch $DFX_ASSETS/binary_cache.tgz
+      export DFX_CONFIG_ROOT="$PWD"
+      export DFX_ASSETS="$PWD/dfx_assets"
+      mkdir -p "$DFX_ASSETS"
+      touch "$DFX_ASSETS/binary_cache.tgz"
       tar -czf "$DFX_ASSETS"/assetstorage_canister.tgz -C $src/src/distributed assetstorage.did assetstorage.wasm
       tar -czf "$DFX_ASSETS"/wallet_canister.tgz -C $src/src/distributed wallet.did wallet.wasm
       tar -czf "$DFX_ASSETS"/ui_canister.tgz -C $src/src/distributed ui.did ui.wasm
