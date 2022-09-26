1def8ba00cb9496ec59b843726be3a4a571dfd83
Paul H. Liu <paul@thev.net>
2022-09-26 00:57:03 -0700

    Update sha256

--- ic.nix
+++ ic.nix
@@ -90,7 +90,7 @@ let
       else
         [ libunwind ]);
       cargoSha256 =
-        "0000000000000000000000000000000000000000000000000000"; # cargoSha256
+        "sha256-CNo1d2t4jgfGG6jmBm/+AZds+JL9V9hy4ZRGRh0mQo0="; # cargoSha256
       doCheck = false;
 
       ROCKSDB_LIB_DIR = "${rocksdb}/lib";
