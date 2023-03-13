3604d468cc9c0642d16b2e11f1170dca91a4f3b4
ninegua <ninegua@users.noreply.github.com>
2023-03-13 04:17:21 +0000

    Update source dependencies

--- nix/sources.nix
+++ nix/sources.nix
@@ -1,8 +1,8 @@
 { fetchgit }: {
   ic = fetchgit {
     url = "https://github.com/dfinity/ic"; # master
-    rev = "8cec935302f157bc20ff9583fd8bc96b5d48b71e";
-    sha256 = "0my8mw0i9xy9r4f5ix52i972pn8rm4cyvs3s7ahz9fzlkcm1338m";
+    rev = "2e27bd630dd32d582b3e8aa941182b212e7f60f0";
+    sha256 = "0lc5bja552zzz6wz95y698bln6g0a98pww0zfhdhydyh9mliqqa4";
   };
   icx-proxy = fetchgit {
     url = "https://github.com/dfinity/icx-proxy"; # master
@@ -16,23 +16,23 @@
   };
   motoko = fetchgit {
     url = "https://github.com/dfinity/motoko"; # master
-    rev = "dcfa0a635ce27b389f16df6ef42aa043662f508a";
-    sha256 = "1yrqg5j2wq9v9v8jbd3337yk6xdwlvmdpfl9c400szsi93wqxj3a";
+    rev = "0bea8a5e225b615693eb9929e2b198bf7a937205";
+    sha256 = "0zqidw58k3g1x8p0sjq4scn452dqfhr0bgj35msbc3fafmsyfldz";
   };
   motoko-base = fetchgit {
     url = "https://github.com/dfinity/motoko-base"; # next-moc
-    rev = "8c70bf452e16a9e81d95b764b636e2bee3fd0b39";
-    sha256 = "101nbb5ws43rlkh96fvdcjzdkhwpl33fsl4bxfnfq10835vyn3kz";
+    rev = "98c5717b283a8ec233907c3a3e92c79e3bbab1b3";
+    sha256 = "1hji6j89p2yk975yxpb0p4xzx4vxa6qa14f0vm1ls0f5jq8ch99g";
   };
   musl-wasi = fetchgit {
     url = "https://github.com/WebAssembly/wasi-libc"; # main
-    rev = "8daaba387ce70a6088afe3916e9e16ed1c2bc630";
-    sha256 = "0818aqsn6clm8nc0adbzah67dkhzjc8z20qfgiwq76690jgnp5d4";
+    rev = "f2a35a454e8472b63831885daacc7f5fadd46747";
+    sha256 = "1k7hkx9q9d93xcdmpnp519znvfffh10xk1y853pdjxnlmxndkw9j";
   };
   sdk = fetchgit {
     url = "https://github.com/dfinity/sdk"; # master
-    rev = "bc2283ede24820dfeada4e69b9f3672e1ff53990";
-    sha256 = "1kh86fflb6pszx0bivx7k0mlix09ffwl81zhwmzy4ci60w0vcpkg";
+    rev = "a19bb11a066e6b324d9750d346e8b56b32ae50fa";
+    sha256 = "0adsv7nlgd5w4qn5rzs94gg7g7hy15lfnlz9nq9n19c9h5b8gs09";
   };
   lmdb = fetchgit {
     url = "https://git.openldap.org/openldap/openldap.git"; # mdb.master
@@ -41,13 +41,13 @@
   };
   vessel = fetchgit {
     url = "https://github.com/dfinity/vessel"; # main
-    rev = "e0688426d630e378eb22b1a6a0fa130a8be95685";
-    sha256 = "0flh8nb5hc0sdfp7kkzqxbqdjclkdczb2qv6d09zbs6y8crp9x1j";
+    rev = "99661e40c4c47110129176ee9ecc61a50f1f60db";
+    sha256 = "0azbr1gz6xlk9dwr5zc451krnvb5p3g2fl7iyjh0zhpylb5v3d4y";
   };
   ic-repl = fetchgit {
     url = "https://github.com/chenyan2002/ic-repl"; # master
-    rev = "9adb8a183d2ef6d4425c5c36b04edd36bab76446";
-    sha256 = "1rbif5v10ri9cvdy6dlhyf3nbbb1dhf8szxfczzmdb5dc4kgjn1g";
+    rev = "58374a36f80d25b1aeefb2e0e7b962cf20fcd755";
+    sha256 = "1100m8s6w7gl2wh255n0d1d8c8jkbplkwzrcxxrr418v3gkd189w";
   };
   candid = fetchgit {
     url = "https://github.com/dfinity/candid"; # master
--- utils.nix
+++ utils.nix
@@ -20,13 +20,13 @@ let
   mkDrv = mkDrv_ [ ];
 in rec {
   icx-proxy = mkDrv "icx-proxy"
-    "sha256-Zv9wBf32sv/bQUo4do+xejil5KQebtS/3rGbiRdomnQ="; # cargoSha256
+    "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   vessel = mkDrv "vessel"
-    "sha256-+gx9kKgS4M+usVWK/sK34/7XFob5Vn4K6Ha5rBJ9Dgs="; # cargoSha256
+    "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   ic-repl = mkDrv "ic-repl"
-    "sha256-mmHgtcExeiQ1J9TzFqDsLyhnbnE7+LKGPAYg1bbXAjM="; # cargoSha256
+    "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   candid = mkDrv "candid"
-    "sha256-3DtEkpyeJi2VcpJOXnZCPvwrQH/C9Tnr4xjZ/X4PQsQ="; # cargoSha256
+    "0000000000000000000000000000000000000000000000000000"; # cargoSha256
 
   shell = icx-proxy;
 }
