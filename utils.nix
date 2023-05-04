838e9500120fa59a46b5415bd01311f68cf15e61
Paul Liu <paul@thev.net>
2023-05-04 10:43:41 +0800

    Update candid source (#54)
    * Update candid source
    * Patch candid to fix error test case

--- utils.nix
+++ utils.nix
@@ -26,8 +26,8 @@ in rec {
     "sha256-WuCYuTXILVDDTk48882ubBDZDKX2z6bqO2bBUy/hDzI="; # cargoSha256
   ic-wasm = mkDrv_ true [ ] "ic-wasm"
     "sha256-4gi8eBI42JggMesDEK/Lo73MWV2UduR8oci0Z0ld89o="; # cargoSha256
-  candid = mkDrv "candid"
-    "sha256-3DtEkpyeJi2VcpJOXnZCPvwrQH/C9Tnr4xjZ/X4PQsQ="; # cargoSha256
+  candid = mkDrv_ false [ ./nix/candid-ignore-test.patch ] "candid"
+    "sha256-dIhtnuMTTsebRFSYibjYauK4kM/kpLk+/ACvljgWKI8="; # cargoSha256
 
   shell = ic-wasm;
 }
