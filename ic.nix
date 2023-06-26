792fb0931bf7249837510dab8286abfc33474e2e
ninegua <ninegua@users.noreply.github.com>
2023-06-26 04:18:23 +0000

    Update source dependencies

--- ic.nix
+++ ic.nix
@@ -94,7 +94,7 @@ let
       else
         [ libunwind-static ]);
       cargoSha256 =
-        "sha256-An45mA5IiZRqKQ6a0JYjQGARsbplXtWRWKWcCJdbR+4="; # cargoSha256
+        "0000000000000000000000000000000000000000000000000000"; # cargoSha256
       doCheck = false;
 
       ROCKSDB_LIB_DIR = "${rocksdb}/lib";
