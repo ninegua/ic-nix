0259c77ee36a39e47a061fcd37b6b77114a07003
Paul H. Liu <paul@thev.net>
2023-11-09 05:58:17 -0800

    Allow use of cargoLock to avoid vendor failure

--- utils.nix
+++ utils.nix
@@ -7,11 +7,12 @@ let
     (with darwin.apple_sdk.frameworks; [ DiskArbitration Foundation ]);
   mkDrv = { doCheck ? true, buildFeatures ? [ ]
     , dontUseCargoParallelTests ? false, cargoPatches ? [ ]
-    , cargoBuildFlags ? "", patchPhase ? "" }:
-    name: cargoSha256:
+    , cargoBuildFlags ? "", postUnpack ? "", cargoLock ? null
+    , cargoSha256 ? null }:
+    name:
     rustPlatform.buildRustPackage {
-      inherit name buildFeatures cargoSha256 cargoPatches doCheck
-        dontUseCargoParallelTests cargoBuildFlags patchPhase;
+      inherit name buildFeatures cargoPatches doCheck dontUseCargoParallelTests
+        cargoBuildFlags postUnpack cargoSha256 cargoLock;
       src = sources."${name}";
       buildInputs = [ openssl-static ] ++ lib.optionals stdenv.isDarwin
         (with darwin.apple_sdk.frameworks; [ SystemConfiguration Security ]);
@@ -19,30 +20,69 @@ let
       RUSTFLAGS = [ "-Clinker=${linker}" "-Lnative=${libcxxabi}/lib" ];
     };
 in rec {
-  icx-proxy =
-    mkDrv { buildFeatures = [ "skip_body_verification" ]; } "icx-proxy"
-    "sha256-RkO9vjpJACsEZjgfQ57/c73EObJRB2l/5R3zt08u+WA="; # cargoSha256
-  idl2json = mkDrv { } "idl2json"
-    "sha256-2btmJ5dvyAmI9eapXiIHeeVn8/FWHsvve3YtoGK3L9k="; # cargoSha256
-  vessel = mkDrv { } "vessel"
-    "sha256-ZmGVRi+7kEqEwHQnwjTLjUElgg544wXlhebqGiW+GE8="; # cargoSha256
-  ic-repl = mkDrv { } "ic-repl"
-    "sha256-z6YSY3lUS4twE2+sFimsJBQceopmyQ+X+1LZ3hGzb3k="; # cargoSha256
-  ic-wasm = mkDrv { dontUseCargoParallelTests = true; } "ic-wasm"
-    "sha256-WfA+cxZ/woDgmbXtO7oeJDCG30vNH0ZslKH/3UJupaU="; # cargoSha256
-  candid = mkDrv { } "candid"
-    "sha256-jsOr5bPp5AzedKa02F7YDVISkuKow9VJjiojtDp0FCM="; # cargoSha256
-  cdk-rs = mkDrv { doCheck = false; } "cdk-rs"
-    "sha256-g3RWRzitfb7cVbyoEPEHhsLf6PUDHL2jN/ky/LZvadI="; # cargoSha256
-  agent-rs = mkDrv { doCheck = false; } "agent-rs"
-    "sha256-tgUu4KDlmhBBdbhT2PZcJHQqhlW6pseZH8tKG/aW7T4="; # cargoSha256
+  icx-proxy = mkDrv {
+    buildFeatures = [ "skip_body_verification" ];
+    cargoSha256 =
+      "sha256-RkO9vjpJACsEZjgfQ57/c73EObJRB2l/5R3zt08u+WA="; # cargoSha256
+  } "icx-proxy";
+
+  idl2json = mkDrv {
+    cargoSha256 =
+      "sha256-2btmJ5dvyAmI9eapXiIHeeVn8/FWHsvve3YtoGK3L9k="; # cargoSha256
+  } "idl2json";
+
+  vessel = mkDrv {
+    cargoSha256 =
+      "sha256-ZmGVRi+7kEqEwHQnwjTLjUElgg544wXlhebqGiW+GE8="; # cargoSha256
+  } "vessel";
+
+  ic-repl = mkDrv {
+    cargoSha256 =
+      "sha256-z6YSY3lUS4twE2+sFimsJBQceopmyQ+X+1LZ3hGzb3k="; # cargoSha256
+  } "ic-repl";
+
+  ic-wasm = mkDrv {
+    dontUseCargoParallelTests = true;
+    cargoSha256 =
+      "sha256-WfA+cxZ/woDgmbXtO7oeJDCG30vNH0ZslKH/3UJupaU="; # cargoSha256
+  } "ic-wasm";
+
+  candid = mkDrv {
+    cargoSha256 =
+      "sha256-jsOr5bPp5AzedKa02F7YDVISkuKow9VJjiojtDp0FCM="; # cargoSha256
+  } "candid";
+
+  cdk-rs = mkDrv {
+    doCheck = false;
+    cargoSha256 =
+      "sha256-g3RWRzitfb7cVbyoEPEHhsLf6PUDHL2jN/ky/LZvadI="; # cargoSha256
+  } "cdk-rs";
+
+  agent-rs = mkDrv {
+    doCheck = false;
+    cargoSha256 =
+      "sha256-tgUu4KDlmhBBdbhT2PZcJHQqhlW6pseZH8tKG/aW7T4="; # cargoSha256
+  } "agent-rs";
+
   dfx-extensions = mkDrv {
     doCheck = false;
-    patchPhase =
-      "rm extensions/{nns,sns}/build.rs; sed -i 's/^build =.*$//' extensions/{nns,sns}/Cargo.toml*";
+    postUnpack = ''
+      pushd $sourceRoot
+      rm extensions/{nns,sns}/build.rs
+      sed -i 's/^build =.*$//' extensions/{nns,sns}/Cargo.toml*
+      popd
+    '';
     cargoBuildFlags = "--bin nns --bin sns";
-  } "dfx-extensions"
-    "sha256-4h/MfCao1EOdERTyKoViK/Lxrym8WPg6t8Y6ViHcuuI="; # cargoSha256
+    cargoLock = {
+      lockFile = "${sources.dfx-extensions}/Cargo.lock";
+      outputHashes = {
+        "dfx-core-0.0.1" =
+          "sha256-g2ab1LM0jE7BDUWTc017+e88cODt/IwdTxs2N/tRPt4=";
+        "ic-agent-0.25.0" =
+          "sha256-QgGYo2qdRge7g6ul3PcTAyLgm+coJV2K1MiptKtFwos=";
+      };
+    };
+  } "dfx-extensions"; # cargoSha256
 
   shell = ic-wasm;
 }
