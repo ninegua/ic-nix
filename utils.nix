ed3906ce154da8ab9d28acc36d48faf33d2dd8ad
ninegua <ninegua@users.noreply.github.com>
2024-04-08 04:16:26 +0000

    Update source dependencies

--- utils.nix
+++ utils.nix
@@ -23,22 +23,22 @@ in rec {
   icx-proxy = mkDrv {
     buildFeatures = [ "skip_body_verification" ];
     cargoSha256 =
-      "sha256-RkO9vjpJACsEZjgfQ57/c73EObJRB2l/5R3zt08u+WA="; # cargoSha256
+      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   } "icx-proxy";
 
   idl2json = mkDrv {
     cargoSha256 =
-      "sha256-2btmJ5dvyAmI9eapXiIHeeVn8/FWHsvve3YtoGK3L9k="; # cargoSha256
+      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   } "idl2json";
 
   vessel = mkDrv {
     cargoSha256 =
-      "sha256-ZmGVRi+7kEqEwHQnwjTLjUElgg544wXlhebqGiW+GE8="; # cargoSha256
+      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   } "vessel";
 
   ic-repl = mkDrv {
     cargoSha256 =
-      "sha256-S08ceZO8orDHc6MELy3rkhfmXvnOB72sMwqFORMx5wE="; # cargoSha256
+      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   } "ic-repl";
 
   ic-wasm = mkDrv {
@@ -46,24 +46,24 @@ in rec {
     dontUseCargoParallelTests = true;
     cargoPatches = [ ];
     cargoSha256 =
-      "sha256-a8iN/lTEVqdmogsSlT3+v3nivSG5VRhOz4/trmAsZLY="; # cargoSha256
+      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   } "ic-wasm";
 
   candid = mkDrv {
     cargoSha256 =
-      "sha256-wv3Nz1nbuGY/amGPsSy2MCvkDKrHa0ZngU2AlJUJek4="; # cargoSha256
+      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   } "candid";
 
   cdk-rs = mkDrv {
     doCheck = false;
     cargoSha256 =
-      "sha256-FKMhOzGkHDjZivYbvYnkVAn2QMgo13/J/7gv5/NzgOE="; # cargoSha256
+      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   } "cdk-rs";
 
   agent-rs = mkDrv {
     doCheck = false;
     cargoSha256 =
-      "sha256-f/xOEBiPUAEhcu96zMl8AjI14iiYQ8xvyUPaxsv4qQA="; # cargoSha256
+      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
   } "agent-rs";
 
   dfx-extensions = mkDrv {
