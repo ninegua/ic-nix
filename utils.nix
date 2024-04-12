9b97bcd88c2c04f2fc0ab8eef614857d5ccb9abd
Paul H. Liu <paul@thev.net>
2024-04-11 18:28:19 -0700

    Fix

--- utils.nix
+++ utils.nix
@@ -23,22 +23,22 @@ in rec {
   icx-proxy = mkDrv {
     buildFeatures = [ "skip_body_verification" ];
     cargoSha256 =
-      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
+      "sha256-RkO9vjpJACsEZjgfQ57/c73EObJRB2l/5R3zt08u+WA="; # cargoSha256
   } "icx-proxy";
 
   idl2json = mkDrv {
     cargoSha256 =
-      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
+      "sha256-q2CVUwoL6VVzuWx2yZMFKb9M/xzEXgPvL584KAQiGj8="; # cargoSha256
   } "idl2json";
 
   vessel = mkDrv {
     cargoSha256 =
-      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
+      "sha256-ZmGVRi+7kEqEwHQnwjTLjUElgg544wXlhebqGiW+GE8="; # cargoSha256
   } "vessel";
 
   ic-repl = mkDrv {
     cargoSha256 =
-      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
+      "sha256-49wREWUht915LwwjhdM7RNip7sbNZ63iD008DgDyVbs="; # cargoSha256
   } "ic-repl";
 
   ic-wasm = mkDrv {
@@ -46,27 +46,27 @@ in rec {
     dontUseCargoParallelTests = true;
     cargoPatches = [ ];
     cargoSha256 =
-      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
+      "sha256-FOI12PBCg9C91ztX7V8HZyfbe6llUHHwtR+rydbHYSo="; # cargoSha256
   } "ic-wasm";
 
   candid = mkDrv {
     cargoSha256 =
-      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
+      "sha256-Keg9JdwqY6/M1bcqSQCXhBxesXrq+dFuS2UL6oSKGFY="; # cargoSha256
   } "candid";
 
   cdk-rs = mkDrv {
     doCheck = false;
     cargoSha256 =
-      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
+      "sha256-Dp/SKpAsfLaqh+lrXXCzHPn24ZkNczZBe61BYjrSsfg="; # cargoSha256
   } "cdk-rs";
 
   agent-rs = mkDrv {
     doCheck = false;
     cargoSha256 =
-      "0000000000000000000000000000000000000000000000000000"; # cargoSha256
+      "sha256-gCThFth0bE7alXomgJlalwKw8flndECPyEhvL4ZVaa8="; # cargoSha256
   } "agent-rs";
 
-  dfx-extensions = mkDrv {
+  dfx-extensions = (mkDrv {
     doCheck = false;
     postUnpack = ''
       pushd $sourceRoot
@@ -78,11 +78,22 @@ in rec {
     cargoLock = {
       lockFile = "${sources.dfx-extensions}/Cargo.lock";
       outputHashes = {
+        "build-info-0.0.27" =
+          "sha256-SkwWwDNrTsntkNiCv6rsyTFGazhpRDnKtVzPpYLKF9U=";
+        "cycles-minting-canister-0.9.0" =
+          "sha256-AGgV8gEFk62aE8WPLDqthjpthhLM1Md/+P+VsCk/Mcc=";
+        "derive_more-0.99.8-alpha.0" =
+          "sha256-tEsfYC9oCAsDjinCsUDgRg3q6ruvayuA1lRmsEP9cys=";
+        "ic-btc-interface-0.2.0" =
+          "sha256-5tXrN56F5EyXQR8FzTV44ZKNxqgBOVAXD8pEKDdnBzw=";
         "dfx-core-0.0.1" =
           "sha256-SdqxP5skgt15cz9j6Yf1HMB7uU8zBnEKJV4nsDbe4zY=";
       };
     };
-  } "dfx-extensions";
+  } "dfx-extensions").overrideAttrs (_: {
+    IC_ICRC1_ARCHIVE_WASM_PATH =
+      "../../ic-icrc1-0.9.0/wasm/ic-icrc1-archive.wasm.gz";
+  });
 
   shell = ic-wasm;
 }
