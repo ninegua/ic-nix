eea7941fcb12168aa2ac0fa9fadfdbf7b71e4180
Paul H. Liu <paul@thev.net>
2024-04-13 01:08:37 -0700

    Add missing icx-proxy to release

--- release.nix
+++ release.nix
@@ -38,7 +38,7 @@ in rec {
     installPhase = ''
         mkdir -p $out/bin $out/share
         cp ${moc}/bin/mo* $out/bin/
-        cp ${ic.binaries}/bin/{replica,ic-admin,ic-prep,ic-starter,ic-btc-adapter,ic-https-outcalls-adapter,ic-nns-init,ic-state-machine-tests,canister_sandbox,sandbox_launcher} $out/bin/
+        cp ${ic.binaries}/bin/{replica,ic-admin,ic-prep,ic-starter,ic-btc-adapter,ic-https-outcalls-adapter,ic-nns-init,ic-state-machine-tests,canister_sandbox,sandbox_launcher,icx-proxy} $out/bin/
         cp ${dfx-extensions}/bin/{nns,sns} $out/bin/
         cp ${ic.binaries}/bin/sns $out/bin/sns-cli
         cp ${dfx}/bin/* $out/bin/
