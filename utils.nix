9a96e3769b20befc8738a5039d6fc9e38a80209f
Paul H. Liu <paul@thev.net>
2024-11-15 07:34:18 -0800

    Fix

--- utils.nix
+++ utils.nix
@@ -37,7 +37,6 @@ in rec {
 
   ic-repl = mkDrv {
     outputHashes = {
-      "ic-agent-0.38.2" = "sha256-eR6bWPsKZgjzY71WRbunuf8uaJmFn41ygzEpkQqsrgs=";
     };
   } "ic-repl";
 
@@ -68,9 +67,7 @@ in rec {
       "build-info-0.0.27" =
         "sha256-SkwWwDNrTsntkNiCv6rsyTFGazhpRDnKtVzPpYLKF9U=";
       "cycles-minting-canister-0.9.0" =
-        "sha256-NlMf9Ok7jEZuhLv7W7UxbRM/tK8YG4UWUm9qqfXJXOw=";
-      "dfx-core-0.0.1" = "sha256-34k78kmdjL9k52W2Fw0CYyd2j3uFzO1g+PsNug4jlUU=";
-      "ic-agent-0.36.0" = "sha256-/0znCHkzjINM8uHXSJgmw0Hk+ltbB1VVlBmrjJc5364=";
+        "sha256-4JDOqUgK74qXC9EbRRHUICBUsJHW+eD3fRy00NmQS/M=";
     };
   } "dfx-extensions").overrideAttrs (_: {
     IC_ICRC1_ARCHIVE_WASM_PATH =
