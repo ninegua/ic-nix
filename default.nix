c7b39eed598742c4d74c140a3f52537a8a951757
Paul H. Liu <paul@thev.net>
2024-12-15 23:01:17 -0800

    Update motoko dependencies

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let
         targets = [ "wasm32-unknown-unknown" ];
         extensions = [ "rust-src" ];
       };
-      rust-nightly = self.rust-bin.nightly."2023-04-21".default.override {
+      rust-nightly = self.rust-bin.nightly."2024-07-28".default.override {
         targets = [ "wasm32-unknown-emscripten" "wasm32-wasi" ];
         extensions = [ "rust-src" ];
       };
